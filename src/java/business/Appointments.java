eccd86ee387aff7ebcb3f69ef552269c7bbf8e9b
Ashley Beck <ab733266@southeast.edu>
2022-10-10 19:51:23 -0500

    added basic packages, started adding database connection

---
+++ src/java/business/Appointments.java
@@ -0,0 +1,141 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package business;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+/**
+ *
+ * @author Ashbb
+ */
+public class Appointments {
+    private int apptID;
+    private LocalDate apptDate;
+    private LocalTime apptTime;
+    private int userID;
+    private String userFirstName;
+    private String userLastName;
+    private String doctorFirstName;
+    private String doctorLastName;
+    private String apptType;
+    private String reasonForVisit;
+    private String insuranceProvider;
+    private String insurancePlanNum;
+
+    public Appointments(int apptID, LocalDate apptDate, LocalTime apptTime, int userID, String userFirstName, String userLastName, String doctorFirstName, String doctorLastName, String apptType, String reasonForVisit, String insuranceProvider, String insurancePlanNum) {
+        this.apptID = apptID;
+        this.apptDate = apptDate;
+        this.apptTime = apptTime;
+        this.userID = userID;
+        this.userFirstName = userFirstName;
+        this.userLastName = userLastName;
+        this.doctorFirstName = doctorFirstName;
+        this.doctorLastName = doctorLastName;
+        this.apptType = apptType;
+        this.reasonForVisit = reasonForVisit;
+        this.insuranceProvider = insuranceProvider;
+        this.insurancePlanNum = insurancePlanNum;
+    }
+
+    public Appointments() {
+    }
+
+    public int getApptID() {
+        return apptID;
+    }
+
+    public void setApptID(int apptID) {
+        this.apptID = apptID;
+    }
+
+    public LocalDate getApptDate() {
+        return apptDate;
+    }
+
+    public void setApptDate(LocalDate apptDate) {
+        this.apptDate = apptDate;
+    }
+
+    public LocalTime getApptTime() {
+        return apptTime;
+    }
+
+    public void setApptTime(LocalTime apptTime) {
+        this.apptTime = apptTime;
+    }
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    public String getUserFirstName() {
+        return userFirstName;
+    }
+
+    public void setUserFirstName(String userFirstName) {
+        this.userFirstName = userFirstName;
+    }
+
+    public String getUserLastName() {
+        return userLastName;
+    }
+
+    public void setUserLastName(String userLastName) {
+        this.userLastName = userLastName;
+    }
+
+    public String getDoctorFirstName() {
+        return doctorFirstName;
+    }
+
+    public void setDoctorFirstName(String doctorFirstName) {
+        this.doctorFirstName = doctorFirstName;
+    }
+
+    public String getDoctorLastName() {
+        return doctorLastName;
+    }
+
+    public void setDoctorLastName(String doctorLastName) {
+        this.doctorLastName = doctorLastName;
+    }
+
+    public String getApptType() {
+        return apptType;
+    }
+
+    public void setApptType(String apptType) {
+        this.apptType = apptType;
+    }
+
+    public String getReasonForVisit() {
+        return reasonForVisit;
+    }
+
+    public void setReasonForVisit(String reasonForVisit) {
+        this.reasonForVisit = reasonForVisit;
+    }
+
+    public String getInsuranceProvider() {
+        return insuranceProvider;
+    }
+
+    public void setInsuranceProvider(String insuranceProvider) {
+        this.insuranceProvider = insuranceProvider;
+    }
+
+    public String getInsurancePlanNum() {
+        return insurancePlanNum;
+    }
+
+    public void setInsurancePlanNum(String insurancePlanNum) {
+        this.insurancePlanNum = insurancePlanNum;
+    }
+}
---
+++ src/java/business/Users.java
@@ -0,0 +1,140 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package business;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Ashbb
+ */
+public class Users implements Serializable {
+    private int userID;
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String city;
+    private String state;
+    private int zipCode;
+    private String phoneNumber;
+    private String email;
+    private String Role;
+    private String userName;
+    private String password;
+
+    public Users() {
+    }
+
+    public Users(int userID, String firstName, String lastName, String address, String city, String state, int zipCode, String phoneNumber, String email, String Role, String userName, String password) {
+        this.userID = userID;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zipCode = zipCode;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+        this.Role = Role;
+        this.userName = userName;
+        this.password = password;
+    }
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public int getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(int zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getRole() {
+        return Role;
+    }
+
+    public void setRole(String Role) {
+        this.Role = Role;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
